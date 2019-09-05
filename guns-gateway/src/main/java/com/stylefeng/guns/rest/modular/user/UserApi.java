bc1467a608e600a283994f834bf08d670ec385d2
夏先鹏 <951256916>
2019-09-06 01:19:50 +0800

    增加服务提供者

---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/user/UserApi.java
@@ -0,0 +1,10 @@
+package com.stylefeng.guns.rest.modular.user;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-09-06 01:14
+ */
+public interface UserApi {
+
+    boolean login(String username, String password);
+}
