e5aad2087706b0a4f46912ca4b53d141e6df63f1
夏先鹏 <951256916>
2019-09-11 01:09:03 +0800

    register

--- guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
+++ guns-user/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -4,6 +4,10 @@ import com.alibaba.dubbo.config.annotation.Service;
 import com.stylefeng.guns.api.user.UserApi;
 import com.stylefeng.guns.api.user.UserInfoModel;
 import com.stylefeng.guns.api.user.UserModel;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.persistence.dao.UserMapper;
+import com.stylefeng.guns.rest.persistence.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,7 +16,10 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @Service(interfaceClass = UserApi.class)
-public class UserImpl implements UserApi {
+public class UserServiceImpl implements UserApi {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public int login(String username, String password) {
@@ -21,7 +28,20 @@ public class UserImpl implements UserApi {
 
     @Override
     public boolean register(UserModel userModel) {
-        return false;
+        User user = new User();
+        user.setUsername(userModel.getUsername());
+        // TODO better: md5 混淆加密
+        String md5Password = MD5Util.encrypt(userModel.getPassword());
+        user.setPassword(md5Password);
+        user.setEmail(userModel.getEmail());
+        user.setAddress(userModel.getAddress());
+        user.setPhone(userModel.getPhone());
+        Integer insert = userMapper.insert(user);
+        if (insert > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
